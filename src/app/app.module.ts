bc8a710b382ad30c2f3e1f0fbdd0effbc3d6f46a
PG <pgtissot15@gmail.com>
2019-09-02 16:37:59 +0200

    Architecture de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule,
+    UiModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
